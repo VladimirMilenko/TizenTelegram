8a75ec048c406a445729df542390ca6233a4a656
Vladimir Milenko <v.milenko@innopolis.ru>
2016-04-19 08:26:49 +0300

    Initial commit
    +Base main view
    +Base chat representation

---
+++ js/project.js
@@ -0,0 +1,18 @@
+function OnChatSelected(chatId) {
+    var id = "#chatDiv" + chatId;
+    $(id).toggleClass("selected");
+    id = "#chatNameDiv" + chatId;
+    $(id).toggleClass("selected");
+//    setTimeout(function(){
+//        DeselectChat(chatId);}
+//               ,50);
+    document.location("chat.html");
+}
+
+function DeselectChat(chatId) {
+   var id = "#chatDiv" + chatId;
+    $(id).toggleClass("selected");
+    id = "#chatNameDiv" + chatId;
+    $(id).toggleClass("selected");
+
+}
